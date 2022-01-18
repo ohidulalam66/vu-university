990d2c5b2a04076e9f1174cb25f058ada178fc53
ohidulalam <mdnoyonctg15@gmail.com>
2022-01-18 20:41:46 +0600

    student detail Model create

---
+++ src/Components/StudentList/CourseCard/CourseCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseCard = ({ studentCourses }) => {
+  return (
+    <>
+      <h1>Course Card</h1>
+    </>
+  )
+}
+
+export default CourseCard
--- src/Components/StudentList/SingleStudent/SingleStudent.jsx
+++ src/Components/StudentList/SingleStudent/SingleStudent.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Modal } from 'react-bootstrap'
+import CourseCard from '../CourseCard/CourseCard'
+import './SingleStudent.css'
 
 const SingleStudent = ({ students }) => {
-  const { name, groupName, gender } = students
+  const { id, name, passingYear, cgpa, groupName, gender } = students
   const [modalShow, setModalShow] = useState(false)
+  const [courseList, setCourseList] = useState([])
+
+  useEffect(() => {
+    const url = 'http://localhost:5000/courses'
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCourseList(data))
+  }, [])
+
+  const gotCourses = courseList.filter(
+    (courses) => courses?.groupName === groupName,
+  )
+
   return (
     <>
       <tr>
         <td>{name}</td>
         <td>{groupName}</td>
-        <td>{gender}</td>
+        <td className="text-uppercase">{gender}</td>
         <td>
-          <Button variant="outline-info" onClick={() => setModalShow(true)}>
+          <Button variant="outline-info" onClick={() => setModalShow(id)}>
             <img
               src="https://img.icons8.com/color/28/000000/info--v1.png"
               alt="..."
@@ -35,13 +50,37 @@ const SingleStudent = ({ students }) => {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <h4>
-              Student Name: <span className="text-success">{name}</span>
-            </h4>
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            className="d-flex justify-content-center align-items-center"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/info--v1.png"
+              alt="..."
+            />
+            <h3>Student Detail</h3>
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body></Modal.Body>
+        <Modal.Body>
+          <h5>
+            Name: <span className="text-success">{name}</span>
+          </h5>
+          <p>
+            <b>C GPA:</b> <span className="text-success">{cgpa}</span>
+            <br />
+            <b>Passing Year:</b>{' '}
+            <span className="text-success">{passingYear}</span>
+            <br />
+            <b>Preferred Group:</b>{' '}
+            <span className="text-success">{groupName}</span>
+          </p>
+          {gotCourses.map((studentCourses) => (
+            <CourseCard
+              key={studentCourses._id}
+              studentCourses={studentCourses}
+            />
+          ))}
+        </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
         </Modal.Footer>
